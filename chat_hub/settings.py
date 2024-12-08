ece09e61d485b5dd99f6e87377381f6bdd0ef252
lawrencelee0205 <lawrencelee0205.dev@gmail.com>
2024-12-09 00:09:18 +0800

    CH-1 Setup docker compose.

--- chat_hub/settings.py
+++ chat_hub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+_n2&378ufh!il+mj%fu-r23+41bz@nv)#pj%#-$m7k@_%(po8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "chat_hub.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
